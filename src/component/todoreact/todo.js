bee499c319a2d03e83970bc84e160512dc1039a5
anish101 <anishmohanty101@gmail.com>
2022-02-23 18:52:37 +0530

    Added useState for adding multiple tasks and display the list of all the task slong with previous tasks added

--- src/component/todoreact/todo.js
+++ src/component/todoreact/todo.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
 const Todo = () => {
+  const [inputdata, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    if (!inputdata) {
+      alert("please add the task");
+    }
+    else {
+      setItems([...items, inputdata])
+      setInputData("");
+    }
+    
+  };
+
   return (
     <>
-
       <div className="main-div">
         <div className="child-div">
           <figure>
@@ -11,21 +24,27 @@ const Todo = () => {
             <figcaption>Add Your List here </figcaption>
           </figure>
           <div className="addItems">
-            <input type="text" placeholder="Type your task here" className="form-control" />
-            <i className="fa fa-plus add-btn"></i>
+            <input type="text" placeholder="Type your task here" className="form-control" value={inputdata} onChange={(event) => setInputData(event.target.value)} />
+            <i className="fa fa-plus add-btn" onClick={addItem}></i>
           </div>
           {/*Show Items*/}
           <div className="showItems">
-            <div className="eachItem">
-              <h3>Apole</h3>
+            {items.map((currTask, index) => {
+              return (
+                <div className="eachItem">
+                  {currTask}
                <div className="todo-btn">
                     <i className="far fa-edit add-btn"></i>
                     <i className="far fa-trash-alt add-btn"></i>
                   </div>
             </div>
-          </div>
-          <div className="showItems">
+              )
+            })}
             
+          </div>
+
+          {/*remove all tasks*/}
+          <div className="showItems">
               <button className="btn effect04" data-sm-link-text="Remove All"><span>Check List</span></button>
             </div>
           </div>
