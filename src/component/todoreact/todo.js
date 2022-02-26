78f97d4391744a4c6b634f40d837bd991c232bbd
anish101 <anishmohanty101@gmail.com>
2022-02-26 19:43:13 +0530

    Added feature of editing tasks and changes in user interface

--- src/component/todoreact/todo.js
+++ src/component/todoreact/todo.js
@@ -1,12 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
+
+//Getting Data from local storage
+
+const getLocalData = () => {
+  const lists = localStorage.getItem("myTodoList");
+
+  if (lists) {
+    return JSON.parse(lists);
+  }
+  else{
+    return [];
+  }
+};
 const Todo = () => {
   const [inputdata, setInputData] = useState("");
-  const [items, setItems] = useState([]);
-
+  const [items, setItems] = useState(getLocalData());
+  const [itemToEdit, setEditItem] = useState("");
+  const [toggleButton, setToggle] = useState(false);
   const addItem = () => {
     if (!inputdata) {
       alert("please add the task");
+    }
+    else if (inputdata && toggleButton)
+    {
+      setItems(items.map((currTask) => {
+        if (currTask.id === itemToEdit) {
+          
+          return { ...currTask, name: inputdata };
+        }
+        return currTask;
+      })
+      );
+     setInputData([]);
+    setEditItem(null);
+    setToggle(false);
     }
     else {
       const newInputdata = {
@@ -26,22 +54,43 @@ const Todo = () => {
      setItems(updatedTasks);
   };
 
+    //Edit tasks
+  const editItem = (TaskId) => {
+    const editedItem = items.find((currTask) => {
+      return currTask.id === TaskId;
+    });
+    
+    setInputData(editedItem.name);
+    setEditItem(TaskId);
+    setToggle(true);
+  };
+
   //Remove All Tasks
   const removeAll = () => { setItems([]) };
 
-  
+  //Adding Local Storage
+  useEffect(() => {
+    localStorage.setItem("myTodoList", JSON.stringify(items))
+  }, [items]);
+
   return (
     <>
       <div className="main-div">
         <div className="child-div">
-          <figure>
-            <img src="./images/todo.svg" alt="todologo" />
-            <figcaption>Add Your List here </figcaption>
+          <figure className="heading">
+            <img src="./images/todo1.png" alt="todologo" />
+            <div>TODO LIST</div>
           </figure>
           <div className="addItems">
             <input type="text" placeholder="Type your task here" className="form-control" value={inputdata} onChange={(event) => setInputData(event.target.value)} />
-            <i className="fa fa-plus add-btn" onClick={addItem}></i>
+            
+            {toggleButton ? (
+              <i className="far fa-edit add-btn" onClick={addItem}></i>
+            ) : (
+              <i className="fa fa-plus add-btn" onClick={addItem}></i>
+            )}
           </div>
+          <div className="countItems"> {items.length} Tasks left</div>
           {/*Show Items*/}
           <div className="showItems">
             {items.map((currTask) => {
@@ -49,7 +98,7 @@ const Todo = () => {
                 <div className="eachItem" key={currTask.id}>
                   {currTask.name}
                <div className="todo-btn">
-                    <i className="far fa-edit add-btn"></i>
+                    <i className="far fa-edit add-btn" onClick={()=> editItem(currTask.id)}></i>
                     <i className="far fa-trash-alt add-btn" onClick={() => deleteItem(currTask.id)}></i>
                   </div>
             </div>
@@ -59,8 +108,8 @@ const Todo = () => {
           </div>
 
           {/*remove all tasks*/}
-          <div className="showItems" >
-              <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll}><span>Check List</span></button>
+          <div className="RemoveItems" >
+              <button className="btn effect04" onClick={removeAll}><span>Remove ALL</span></button>
             </div>
           </div>
         </div>
